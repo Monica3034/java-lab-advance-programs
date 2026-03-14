b3d0b12d85e721658faa26f32240ca54dd202d4a
MONICA D <monica070505@gmail.com>
2026-03-14 19:30:08 +0530

    Add simple calculator implementation in Calc class

---
+++ simple cal.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Calc
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        System.out.println(s.nextInt() + s.nextInt());
+    }
+}
