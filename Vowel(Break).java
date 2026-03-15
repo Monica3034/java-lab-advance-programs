1c8ae295040a1871bd83b3ff0005d4c74c3f4b6b
MONICA D <monica070505@gmail.com>
2026-03-15 15:40:54 +0530

    Add StopAtVowel class to detect first vowel

---
+++ Vowel(Break).java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StopAtVowel
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+
+        for(int i = 0; i < name.length(); i++)
+        {
+            char ch = name.charAt(i);
+
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+               ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+            {
+                System.out.println("Vowel found: " + ch);
+                break;
+            }
+
+            System.out.println(ch);
+        }
+    }
+}
