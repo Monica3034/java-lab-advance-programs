adc8277cd48a62048653f4b906b66efb6a151ede
MONICA D <monica070505@gmail.com>
2026-03-15 14:47:26 +0530

    Add IncrementDecrement class for pre and post operations

---
+++ Inc&dec using pre and post.java
@@ -0,0 +1,15 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        System.out.println("Initial value: " + a);
+
+        System.out.println("Post Increment: " + a++);
+        System.out.println("Value after Post Increment: " + a);
+
+        System.out.println("Pre Decrement: " + --a);
+        System.out.println("Value after Pre Decrement: " + a);
+
+    }
+}
