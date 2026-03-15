a66cd5964531d8c1f52f031a71328ca7e7cd5d06
MONICA D <monica070505@gmail.com>
2026-03-15 14:31:01 +0530

    Add Decrement class to demonstrate decrement operation

---
+++ Decrement.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Decrement{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a=5;a--;
+        System.out.println("Value of a:" +a);
+    }
+}
