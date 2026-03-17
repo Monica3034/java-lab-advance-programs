8aeedf38a280004ea44252f21b37a7cae50f9b09
MONICA D <monica070505@gmail.com>
2026-03-17 20:03:52 +0530

    Add pattern printing program in Java

---
+++ Pattern(Alpha(Reverse)).java
@@ -0,0 +1,14 @@
+public class Pattern{
+   public static void main(String[]args){
+       int n=5;
+     
+       for(int i=n;i>=1;i--){
+           char ch = 'A';
+           for(int j=1;j<=i;j++){
+               System.out.print(ch+ " ");
+               ch++;
+       }
+       System.out.println();
+     }
+   }
+ }
