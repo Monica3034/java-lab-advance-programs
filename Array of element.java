4d44d681e96b068f5c7b4f462ee1536f70d23020
MONICA D <monica070505@gmail.com>
2026-03-18 20:18:46 +0530

    Add Array class to input and display elements

---
+++ Array of element.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Array{
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        int arr[]=new int[5];
+        System.out.println("Enter the elements:");
+        for(int i=0;i<5;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Array elements are:");
+        for(int i=0;i<5;i++){
+            System.out.println(arr[i]+ " ");
+        }
+    }
+}
