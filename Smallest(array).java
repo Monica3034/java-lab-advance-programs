071d5e41187f877502f7cba81ef74d3bd18706ce
MONICA D <monica070505@gmail.com>
2026-03-18 20:30:55 +0530

    Add Java program to find smallest element in array
    Implement a Java program to find the smallest element in an array.

---
+++ Smallest(array).java
@@ -0,0 +1,13 @@
+class Array{
+    public static void main(String[]args){
+        int arr[]={12,45,67,23,89,10};
+        int min=arr[0];
+        for (int i=1;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+            System.out.println("Smallest element is:" +min);
+    
+    }
+}
