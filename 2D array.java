d3fe83bc905a88a908ca449f32a78e4371019f55
MONICA D <monica070505@gmail.com>
2026-04-15 19:21:58 +0530

    Add TwoDarray class to print a 2D matrix

---
+++ 2D array.java
@@ -0,0 +1,14 @@
+public class TwoDarray{
+    public static void main(String[]args){
+        int matrix[][]={{10,20,40},{30,50,60}};
+       
+        System.out.println("Matrix Element");
+       
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix[i].length;j++){
+                System.out.print(matrix[i][j]+ " ");
+        }
+        System.out.println();
+        }
+    }
+}
